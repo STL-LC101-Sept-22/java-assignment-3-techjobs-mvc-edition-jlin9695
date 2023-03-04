24b87872dd91d51cba67440458eb14e0fa6833c3
Josh Lin <jlin9695@gmail.com>
2023-03-04 02:46:10 -0600

    Search controller.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -22,5 +22,10 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    public String displaySearchResults(Model model){
+        return "search";
+    }
+
+
 
 }
